1a4a4c28c400b2717c8968e5f86f3146634c01e5
K4MIN4R <GhostPL1223@o2.pl>
2024-05-25 23:15:18 +0200

    Update main.c
    r zmienione na F1

--- untitled1/main.c
+++ untitled1/main.c
@@ -154,7 +154,7 @@ void drawEndgameScreen(){
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, endgameText[i]);
     }
 
-    char restartText[] = "Press R to Restart";
+    char restartText[] = "Press F1 to Restart";
     glRasterPos2i(COLS * CELL_SIZE / 2 - 80, (ROWS + UI_HEIGHT) * CELL_SIZE / 2 - 20);
     len = strlen(restartText);
     for (int i = 0; i < len; i++){
